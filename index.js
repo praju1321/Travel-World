3cf30438f103b8aceef5b2c6c4e6ad6ec0484317
Prajwal Rajhans Narnaware <89122058+praju1321@users.noreply.github.com>
2022-02-12 21:32:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var menuToggle = document.querySelector('.toggle');
+var showcase = document.querySelector('.showcase');
+
+menuToggle.addEventListener('click', function() {
+  menuToggle.classList.toggle('active');
+  showcase.classList.toggle('active');
+})
